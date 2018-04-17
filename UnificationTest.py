379b90ffdb2cc47f2657f225145a86c025b879a9
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-04-17 15:43:58 +0100

    done number

--- UnificationTest.py
+++ UnificationTest.py
@@ -2,29 +2,32 @@ from nltk.grammar import FeatureGrammar
 from nltk import FeatureChartParser
 
 ugrammar = FeatureGrammar.fromstring("""
-S -> NP[NUM=?n] VP[FORM=?f, NUM=?n, SUBCAT=nil] | PP S | WH Aux NP[NUM=?n] VP[FORM=?f, NUM=?n, SUBCAT=nil]
-NP[NUM=?n] -> ProperNoun[NUM=sing] | NOUN[NUM=?n] | NP[NUM=?n] CONJ NP[NUM=?n] | DET[NUM=?n] Nominal
+S -> NP[NUM=?n] VP[FORM=?f, NUM=?n, SUBCAT=nil] | PP S | WH Aux[NUM=?n] NP[NUM=?n] VP[FORM=?f, NUM=?n, SUBCAT=nil]
+NP[NUM=?n] -> ProperNoun[NUM=?n] | Nominal | DET[NUM=?n] Nominal
+NP[NUM=pl] -> NP[NUM=?n] CP
 VP[FORM=?f, NUM=?n, SUBCAT=?rest] -> VP[FORM=?f, NUM=?n, SUBCAT=[HEAD=?arg, TAIL=?rest]] ARG[CAT=?arg]
 VP[FORM=?f, NUM=?n, SUBCAT=?rest] -> VP[FORM=?f, NUM=?n, SUBCAT=[HEAD=?arg, TAIL=?rest]] S
 VP[FORM=?f, NUM=?n, SUBCAT=?args] -> V[FORM=?f, NUM=?n, SUBCAT=?args] | ADV V[FORM=?f, NUM=?n, SUBCAT=?args]
 ARG[CAT=np] -> NP[NUM=?n]
 ARG[CAT=pp] -> PP
-ARG[CAT=nominal] -> Nominal
+CP -> CONJ NP[NUM=?n]
 PP -> Prep NP[NUM=?n] | CONJ S
-Nominal -> NOUN[NUM=?n] | Nominal PP | ADJ Nominal | DET[NUM=?n] Nominal | Nominal NOUN[NUM=?n]
-ProperNoun[NUM=sing] -> 'Bart' | 'Homer' | 'Lisa'
+Nominal -> NOUN[NUM=?n] | Nominal PP | ADJ Nominal | Nominal NOUN[NUM=?n]
+ProperNoun[NUM=sg] -> 'Bart' | 'Homer' | 'Lisa'
 CONJ -> 'and' | 'when'
 ADV -> 'always' | 'never'
-Aux -> 'do' | 'does'
-V[FORM=base, NUM=plur, SUBCAT=nil] -> 'laugh'
-V[FORM=base, NUM=plur, SUBCAT=[HEAD=np, TAIL=nil]] -> 'drink' | 'wear' | 'laugh'
-V[FORM=vbz, NUM=sing, SUBCAT=[HEAD=?arg, TAIL=?rest]] -> 'wears' | 'serves' | 'drinks' | 'thinks' | 'laughs'
-V[FORM=vbz, NUM=sing, SUBCAT=nil] -> 'laughs'
+Aux[NUM=pl] -> 'do'
+Aux[NUM=sg] -> 'does'
+V[FORM=base, NUM=pl, SUBCAT=nil] -> 'laugh'
+V[FORM=base, NUM=pl, SUBCAT=[HEAD=np, TAIL=nil]] -> 'drink' | 'wear'
+V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=?arg, TAIL=nil]] -> 'wears' | 'serves' | 'drinks' | 'thinks'
+V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=?arg, TAIL=[HEAD=?arg, TAIL=nil]]] -> 'wears' | 'serves' | 'drinks' | 'thinks'
+V[FORM=vbz, NUM=sg, SUBCAT=nil] -> 'laughs'
 V[FORM=pret, NUM=?n, SUBCAT=nil] -> 'laughed'
-DET[NUM=sing] -> 'a'
+DET[NUM=sg] -> 'a'
 DET[NUM=?n] -> 'the'
-NOUN[NUM=sing] -> 'milk' | 'salad' | 'kitchen' | 'midnight' | 'table'
-NOUN[NUM=plur] -> 'shoes'
+NOUN[NUM=sg] -> 'milk' | 'salad' | 'kitchen' | 'midnight' | 'table'
+NOUN[NUM=pl] -> 'shoes'
 ADJ -> 'blue' | 'healthy' | 'green'
 Prep -> 'in' | 'before' | 'on'
 WH -> 'when'
