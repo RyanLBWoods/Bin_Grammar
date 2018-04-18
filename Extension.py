661b3abf622c695592170baef361ebefff7740bd
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-04-18 12:54:41 +0100

    finish parsing

--- Extension.py
+++ Extension.py
@@ -10,8 +10,8 @@ S -> NP[NUM=?n] VP[FORM=?f, NUM=?n, SUBCAT=nil] | PP S
 S -> NP[NUM=?n] MVP[FORM=?f, NUM=?n, SUBCAT=nil]
 S -> WhNP InvS
 S -> WH InvS
-InvS -> Aux[NUM=?n] S[NUM=pl]
-S[NUM=pl] -> NP[NUM=?n] VP[FORM=?f, NUM=pl, SUBCAT=nil]
+InvS -> Aux[NUM=?n] IS[NUM=pl]
+IS[NUM=pl] -> NP[NUM=?n] VP[FORM=?f, NUM=pl, SUBCAT=nil]
 # Noun Phrase expansion productions
 NP[NUM=?n] -> ProperNoun[NUM=?n] | ProNoun[NUM=?n] | Nominal | DET[NUM=?n] Nominal | NP[NUM=?n] GerundNP
 NP[NUM=pl] -> NP[NUM=?n] CP
@@ -60,7 +60,8 @@ V[FORM=pret, NUM=?n, SUBCAT=nil] -> 'laughed'
 # VT + NP
 V[FORM=base, NUM=pl, SUBCAT=[HEAD=np, TAIL=nil]] -> 'drink' | 'wear' | 'serve'
 V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=np, TAIL=nil]] -> 'wears' | 'serves' | 'drinks' | 'thinks' | 'likes'
-V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=subc, TAIL=nil]] -> 'thinks' | 'think'
+V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=subc, TAIL=nil]] -> 'thinks'
+V[FORM=vbz, NUM=pl, SUBCAT=[HEAD=subc, TAIL=nil]] -> 'think'
 V[FORM=past, NUM=?n, SUBCAT=[HEAD=np, TAIL=nil]] -> 'drunk' | 'seen'
 V[FORM=presP, NUM=?n, SUBCAT=[HEAD=np, TAIL=nil]] -> 'drinking'
 # VT + NP + PP
@@ -85,24 +86,23 @@ what salad does Bart serve
 whom does Homer serve salad
 whom do Homer and Lisa serve
 what salad does Bart think Homer serves Lisa
+Bart laughs
+Homer laughed
+Bart and Lisa drink milk
+Bart wears blue shoes
+Lisa serves Bart a healthy green salad
+Homer serves Lisa
+Bart always drinks milk
+Lisa thinks Homer thinks Bart drinks milk
+Homer never drinks milk in the kitchen before midnight
+when Homer drinks milk Bart laughs
+when does Lisa drink the milk on the table
+when do Lisa and Bart wear shoes
+Bart laugh
+when do Homer drinks milk
+Bart laughs the kitchen
+he puts bread on the table
 """
-# Bart laughs
-# Homer laughed
-# Bart and Lisa drink milk
-# Bart wears blue shoes
-# Lisa serves Bart a healthy green salad
-# Homer serves Lisa
-# Bart always drinks milk
-# Lisa thinks Homer thinks Bart drinks milk
-# Homer never drinks milk in the kitchen before midnight
-# when Homer drinks milk Bart laughs
-# when does Lisa drink the milk on the table
-# when do Lisa and Bart wear shoes
-# Bart laugh
-# when do Homer drinks milk
-# Bart laughs the kitchen
-# he puts bread on the table
-# """
 
 sents = text.splitlines()
 for sent in sents:
