603b959dff69d6b4dd970f30651ddeeb03230e74
Lin Bin <linbin@8afbcf31.cs.st-andrews.ac.uk>
2018-04-18 13:03:41 +0100

    adverbial clause test

--- UnificationTest.py
+++ UnificationTest.py
@@ -1,42 +1,67 @@
 from nltk.grammar import FeatureGrammar
 from nltk import FeatureChartParser
 
-ugrammar = FeatureGrammar.fromstring("""
-S -> NP[NUM=?n] VP[FORM=?f, NUM=?n, SUBCAT=nil] | PP S | WH Aux[NUM=?n] NP[NUM=?n] VP[FORM=?f, NUM=?n, SUBCAT=nil]
-NP[NUM=?n] -> ProperNoun[NUM=?n] | Nominal | DET[NUM=?n] Nominal
+ugrammar = FeatureGrammar.fromstring("""\
+# ###################
+# Grammar Productions
+# ###################
+# Sentence expansion productions
+S -> NP[NUM=?n] VP[FORM=?f, NUM=?n, SUBCAT=nil] | AdvC S | WH Aux[NUM=?n] NP[NUM=?n] VP[FORM=?f, NUM=pl, SUBCAT=nil]
+# Noun Phrase expansion productions
+NP[NUM=?n] -> ProperNoun[NUM=?n] | ProNoun[NUM=?n] | Nominal | DET[NUM=?n] Nominal
 NP[NUM=pl] -> NP[NUM=?n] CP
+# Verb Phrase expansion productions
 VP[FORM=?f, NUM=?n, SUBCAT=?rest] -> VP[FORM=?f, NUM=?n, SUBCAT=[HEAD=?arg, TAIL=?rest]] ARG[CAT=?arg]
-VP[FORM=?f, NUM=?n, SUBCAT=?rest] -> VP[FORM=?f, NUM=?n, SUBCAT=[HEAD=?arg, TAIL=?rest]] S
 VP[FORM=?f, NUM=?n, SUBCAT=?args] -> V[FORM=?f, NUM=?n, SUBCAT=?args] | ADV V[FORM=?f, NUM=?n, SUBCAT=?args]
+# Argument expansion productions
 ARG[CAT=np] -> NP[NUM=?n]
 ARG[CAT=pp] -> PP
+ARG[CAT=subc] -> S
+# Conjunction Phrase expansion productions
 CP -> CONJ NP[NUM=?n]
-PP -> Prep NP[NUM=?n] | CONJ S
+# Prepositional Phrase expansion productions
+PP -> Prep NP[NUM=?n]
+AdvC -> CONJ S
+# Nominal expansion productions
 Nominal -> NOUN[NUM=?n] | Nominal PP | ADJ Nominal | Nominal NOUN[NUM=?n]
-ProperNoun[NUM=sg] -> 'Bart' | 'Homer' | 'Lisa'
-CONJ -> 'and' | 'when'
-ADV -> 'always' | 'never'
-Aux[NUM=pl] -> 'do'
-Aux[NUM=sg] -> 'does'
-V[FORM=base, NUM=pl, SUBCAT=nil] -> 'laugh'
-V[FORM=base, NUM=pl, SUBCAT=[HEAD=np, TAIL=nil]] -> 'drink' | 'wear'
-V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=?arg, TAIL=nil]] -> 'wears' | 'serves' | 'drinks' | 'thinks'
-V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=?arg, TAIL=[HEAD=?arg, TAIL=nil]]] -> 'wears' | 'serves' | 'drinks' | 'thinks'
-V[FORM=vbz, NUM=sg, SUBCAT=nil] -> 'laughs'
-V[FORM=pret, NUM=?n, SUBCAT=nil] -> 'laughed'
+# ###################
+# Lexical Productions
+# ###################
+# Determiners and Nouns
 DET[NUM=sg] -> 'a'
 DET[NUM=?n] -> 'the'
-NOUN[NUM=sg] -> 'milk' | 'salad' | 'kitchen' | 'midnight' | 'table'
+ProperNoun[NUM=sg] -> 'Bart' | 'Homer' | 'Lisa'
+ProNoun[NUM=sg] -> 'he'
+NOUN[NUM=sg] -> 'milk' | 'salad' | 'kitchen' | 'midnight' | 'table' | 'bread'
 NOUN[NUM=pl] -> 'shoes'
+# Conjunctions and Adverbs
+CONJ -> 'and' | 'when'
+ADV -> 'always' | 'never'
+# Auxiliaries
+Aux[NUM=pl] -> 'do'
+Aux[NUM=sg] -> 'does'
+# ###################
+# Verbs
+# VI
+V[FORM=base, NUM=pl, SUBCAT=nil] -> 'laugh'
+V[FORM=vbz, NUM=sg, SUBCAT=nil] -> 'laughs'
+V[FORM=pret, NUM=?n, SUBCAT=nil] -> 'laughed'
+# VT + NP
+V[FORM=base, NUM=pl, SUBCAT=[HEAD=np, TAIL=nil]] -> 'drink' | 'wear'
+V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=np, TAIL=nil]] -> 'wears' | 'serves' | 'drinks'
+V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=subc, TAIL=nil]] -> 'thinks'
+# VT + NP + PP
+V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=np, TAIL=[HEAD=pp, TAIL=nil]]] -> 'serves' | 'drinks' | 'puts'
+# VT + NP + NP
+V[FORM=vbz, NUM=sg, SUBCAT=[HEAD=np, TAIL=[HEAD=np, TAIL=nil]]] -> 'serves'
+# Adjectives, Prepositions and WHs
 ADJ -> 'blue' | 'healthy' | 'green'
 Prep -> 'in' | 'before' | 'on'
 WH -> 'when'
 """)
 
 uparser = FeatureChartParser(ugrammar)
-text = """
-Lisa drinks milk in the kitchen
-Lisa drinks milk
+text = """\
 Bart laughs
 Homer laughed
 Bart and Lisa drink milk
@@ -47,11 +72,12 @@ Bart always drinks milk
 Lisa thinks Homer thinks Bart drinks milk
 Homer never drinks milk in the kitchen before midnight
 when Homer drinks milk Bart laughs
-when does Lisa drinks the milk on the table
+when does Lisa drink the milk on the table
 when do Lisa and Bart wear shoes
 Bart laugh
 when do Homer drinks milk
 Bart laughs the kitchen
+he puts bread on the table
 """
 
 sents = text.splitlines()
